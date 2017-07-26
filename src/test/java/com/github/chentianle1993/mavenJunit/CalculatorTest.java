58437da9cf488750aa2490eb3960d85fb8d08a7b
chentianle <chentianle@cnic>
2017-07-26 21:29:13 +0800

    CalculatorTest.testAdd() 完成

---
+++ src/test/java/com/github/chentianle1993/mavenJunit/CalculatorTest.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.github.chentianle1993.mavenJunit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author chentianle
+ *
+ */
+public class CalculatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.github.chentianle1993.mavenJunit.Calculator#add(int, int)}.
+	 */
+	@Test
+	public final void testAdd() {
+		//fail("Not yet implemented"); // TODO
+		assertEquals(6, new Calculator().add(1, 5));
+	}
+
+}
